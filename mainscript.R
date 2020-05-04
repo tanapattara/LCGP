e57d386e43a6f68f2707f536db087cd321b447a9
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2020-05-04 23:12:24 +0700

    fix data managerment

--- mainscript.R
+++ mainscript.R
@@ -10,13 +10,17 @@
 # load data
 load_data <- function() {
   library(readr)
-  x <- read_delim("Data/checkins.dat", "|", escape_double = FALSE, trim_ws = TRUE)
+  x <- read_delim("G:/My Drive/Research/Data/Dataset/data_umn_foursquare_datasets/checkins.dat", "|", escape_double = FALSE, trim_ws = TRUE)
   data_checkins = x[-1,]
   
-  x <- read_delim("Data/socialgraph.dat", "|", escape_double = FALSE, trim_ws = TRUE)
-  data_socialgraph = x[-1,]
+  x <- read_delim("G:/My Drive/Research/Data/Dataset/data_umn_foursquare_datasets/socialgraph.dat", "|",
+                  escape_double = FALSE, 
+                  col_types = cols(first_user_id = col_integer(), second_user_id = col_integer()), 
+                  trim_ws = TRUE)
   
-  x <- read_delim("Data/venues.dat", "|", escape_double = FALSE, trim_ws = TRUE)
+  data_socialgraph <- x[rowSums(is.na(x)) != ncol(x),]
+  
+  x <- read_delim("G:/My Drive/Research/Data/Dataset/data_umn_foursquare_datasets/venues.dat", "|", escape_double = FALSE, trim_ws = TRUE)
   data_venues = x[-1,]
   
   rm(x)
@@ -26,25 +30,30 @@ load_data <- function() {
   user.freq <- data.frame(table(data_checkins$user_id))
   colnames(user.freq) <- c("user_id", "freq")
   # filter user checkin frequency more than 10
-  user.freq <- user.freq[(user.freq$freq > 10),]
-  
-  # merge 2 df base from user_id
-  checkins <- merge(data_checkins,user.freq, by.data_checkins = "user_id", by.user.freq = "user_id")
-  # remove column id
-  checkins <- subset(checkins,select = -c(id))
+  user.freq <- user.freq[(user.freq$freq > 9),]
   
   ### Popular location: 
   # Screen out locations with less than 5 check-ins;
   venue.freq <- data.frame(table(data_checkins$venue_id))
   colnames(venue.freq) <- c("venue_id", "freq")
-  # filter user checkin frequency less than 5 check-ins
-  venue.freq <- venue.freq[(venue.freq$freq < 5),]
+  # filter user checkin frequency more than 5 check-ins
+  venue.freq <- venue.freq[(venue.freq$freq > 4),]
+  
+  ### Social contact degree: 
+  socialgraph.firstuser.freq <- data.frame(table(data_socialgraph$first_user_id))
+  colnames(socialgraph.firstuser.freq) <- c("user_id", "freq")
+  # filter user relation frequency more than 30 check-ins
+  socialgraph.firstuser.freq <- socialgraph.firstuser.freq[(socialgraph.firstuser.freq$freq > 30),]
+  
+  
   
   # merge 2 df base from user_id
   checkins <- merge(data_checkins,user.freq, by.data_checkins = "user_id", by.user.freq = "user_id")
   # remove column id
   checkins <- subset(checkins,select = -c(id))
+  # merge 2 df base from user_id
+  checkins <- merge(data_checkins,user.freq, by.data_checkins = "user_id", by.user.freq = "user_id")
+  # remove column id
+  checkins <- subset(checkins,select = -c(id))
   
-  ### Social contact degree: 
-  # Select users with more than 30 social contacts;
 }
